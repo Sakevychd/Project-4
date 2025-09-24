42cac83a57572e6c7506ac08ec9841e3126c90cd
Sakevychd <147507747+Sakevychd@users.noreply.github.com>
2025-09-24 16:03:36 +0300

    Update main.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import time_series_visualizer
-from unittest import main
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
 
-# Test your function by calling it here
-time_series_visualizer.draw_line_plot()
-time_series_visualizer.draw_bar_plot()
-time_series_visualizer.draw_box_plot()
+# 1. Лінійний графік
+fig_line = draw_line_plot()
+print("Лінійний графік збережено як line_plot.png")
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+# 2. Стовпчикова діаграма
+fig_bar = draw_bar_plot()
+print("Стовпчикова діаграма збережена як bar_plot.png")
+
+# 3. Коробкові графіки
+fig_box = draw_box_plot()
+print("Коробкові графіки збережені як box_plot.png")
+
+# 4. Опційно: відобразити графіки
+import matplotlib.pyplot as plt
+fig_line.show()
+fig_bar.show()
+fig_box.show()
